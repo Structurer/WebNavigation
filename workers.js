dbf729e38dd4492e9d2f59ae47a624b5fd1083ad
Ding <113046696+Structurer@users.noreply.github.com>
2023-11-23 20:29:45 +0800

    Update workers.js

--- workers.js
+++ workers.js
@@ -4,39 +4,64 @@ addEventListener('fetch', event => {
 
 async function handleRequest(request) {
   const htmlContent = `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
+  <!DOCTYPE html>
+  <html lang="en">
+
+  <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>4321.run-愿美好如期而至</title>
-    </head>
-    <body>
+  
+      <style>
+        body {
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+          align-items: center;
+          height: 100vh;
+          margin: 0;
+        }
+      
+      
+          h1 {
+              color: #333;
+          }
+  
+          a {
+              text-decoration: none;
+              color: #0066cc;
+              font-weight: bold;
+          }
+  
+          a:hover {
+              color: #ff3300;
+          }
+  
+      </style>
+  </head>
 
+  <body>
+  
       <h1>4321.run</h1>
-      
-      
-      <a href="https://www.Google.com" > Google </a>
-      <br>
-      <a href="https://www.youtube.com" > YouTube </a>
-      <br>
-      <a href="https://chat.openai.com" > chatGPT </a>
-      <br>
-      <a href="https://www.wikipedia.org" > wikipedia </a>
-      <br>
-      
+  
+      <div style="display: flex; justify-content: space-between; width: 62%;">
+        <a href="https://www.Google.com"> Google </a>
+        <a href="https://www.youtube.com"> YouTube </a>
+        <a href="https://chat.openai.com"> chatGPT </a>
+        <a href="https://www.wikipedia.org"> Wikipedia </a>
+      </div>
 
-      <br>
-      <a href="https://www.bilibili.com" > bilibili </a>
-      <br>
-      <a href="https://www.baidu.com" > baidu </a>
-      <br>
-      <a href="https://www.zhihu.com" > zhihu </a>
-      <br>
-      <a href="https://weread.qq.com" > WeRead </a>
+      <div style="display: flex; justify-content: space-between; width: 62%;">
+        <a href="https://www.bilibili.com" > bilibili </a>
+        <a href="https://www.baidu.com" > baidu </a>
+        <a href="https://www.zhihu.com" > zhihu </a>
+        <a href="https://weread.qq.com" > WeRead </a>
+      </div>
+   </div>
 
-    </body>
-    </html>
+  </body>
+  </html>
+  
   `;
 
   return new Response(htmlContent, {
